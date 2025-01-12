490564ab80b2bc5504cdbbafa5fd176bb2ba56fa
Shifard <espallardo.je@gmail.com>
2025-01-12 15:24:11 +0800

    Add functionality for rps project

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let round = 5;
-let computerChoice = ''
+let humanScore = +0;
+let computerScore = +0;
+let description = ''
+
 function getComputerChoice (computerChoice) {
      computerChoice = Math.random();
     if (computerChoice <= 0.33 && computerChoice >= 0) {
@@ -9,61 +11,57 @@ function getComputerChoice (computerChoice) {
     } else if (computerChoice <= 0.99 && computerChoice > 0.66){
         return computerChoice = "scissors";
     }
-}
-// console.log(getComputerChoice(computerChoice).toLowerCase());
+} 
 
-let humanChoice = prompt("Enter either rock, paper, or scissors (in any casing): ");
-function getHumanChoice (humanChoice) {
-    if (round < 5) {
-        humanChoice = prompt("Enter either rock, paper, or scissors (in any casing): ");
-    }
-    choice = humanChoice.toLowerCase();
-    if (choice == 'rock' || choice == 'paper' || choice == 'scissors'){
-        return humanChoice
-    } else {
-        return humanChoice = "Invalid choice"
-    }
-}
-
-
-let humanScore = +0;
-let computerScore = +0;
-let description = ''
-
+let humanChoice = '';
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        humanChoice = button.id;
+        document.getElementById("result").innerHTML = playRound(getComputerChoice(), humanChoice);
+    })
+})
     function playRound(computerChoice, humanChoice) {
-        humanChoice = humanChoice.toLowerCase();
+        if (computerScore == 5 || humanScore == 5) {
+            return description = 'ENDED'
+        } else {
         if (computerChoice == 'rock' && humanChoice == 'rock' 
             || computerChoice == 'paper' && humanChoice == 'paper'
             || computerChoice == 'scissors' && humanChoice == 'scissors'
         ) {
             return description = `Tie! 
             Computer: ${computerScore} \t Human: ${humanScore}`
-        } else if (computerChoice == 'rock' && humanChoice == 'scissors'
+        } else if ((computerChoice == 'rock' && humanChoice == 'scissors'
             || computerChoice == 'paper' && humanChoice == 'rock'
-            || computerChoice == 'scissors' && humanChoice == 'paper'
+            || computerChoice == 'scissors' && humanChoice == 'paper') && computerScore < 4
         ) {
             computerScore +=1;
             return description = `You lose! ${computerChoice} beats ${humanChoice}
             Computer: ${computerScore} \t Human: ${humanScore}`
-        } else if (computerChoice == 'rock' && humanChoice == 'paper'
+        } else if ((computerChoice == 'rock' && humanChoice == 'scissors'
+            || computerChoice == 'paper' && humanChoice == 'rock'
+            || computerChoice == 'scissors' && humanChoice == 'paper') && computerScore == 4
+        ) {
+            computerScore +=1;
+            return description = 'You lose!'
+        } else if ((computerChoice == 'rock' && humanChoice == 'paper'
             || computerChoice == 'paper' && humanChoice == 'scissors'
-            || computerChoice == 'scissors' && humanChoice == 'rock'
+            || computerChoice == 'scissors' && humanChoice == 'rock') && humanScore < 4
         ){
             humanScore +=1;
             return description = `You Win! ${humanChoice} beats ${computerChoice}
             Computer: ${computerScore} \t Human: ${humanScore}`
-        } else {
-            return humanChoice;
+        }
+        else if ((computerChoice == 'rock' && humanChoice == 'paper'
+            || computerChoice == 'paper' && humanChoice == 'scissors'
+            || computerChoice == 'scissors' && humanChoice == 'rock') && humanScore == 4
+        ){
+            humanScore +=1;
+            return description = 'You Win!'
         }
     }
 
-let humanSelection = getHumanChoice(humanChoice);
-let computerSelection = getComputerChoice(computerChoice);
-    while(round!=0){
-        console.log(playRound(getComputerChoice(computerChoice), getHumanChoice(humanChoice)));
-        round --;
 }
-
 
 
 
